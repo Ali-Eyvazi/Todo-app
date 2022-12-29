ad0475b83c3b5780def2def9c7ae5f31ff47c4d2
@li <ali.eyvazi.official@gmail.com>
2022-12-29 13:59:02 +0330

    creating template directory and showing some messages to user

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
 
-        path('hello/', views.say_hello)
+        path('hello/', views.say_hello),
+        path('',views.home)
 ]
--- home/views.py
+++ home/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 # Create your views here.
 
 
 def say_hello(request):
-    return HttpResponse('Hello User...')
+    return render(request, 'hello.html')
 
+
+def home(request):
+    return render(request, 'home.html')
